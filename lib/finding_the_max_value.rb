341c6fbbafc7d2d58de4eb5e4261f548b5a5a472
Sonya Gould <sonyavgould@gmail.com>
2020-06-08 00:19:10 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,17 +2,10 @@ def find_max_value(array)
   count = 0
   highest_number = -1
   while count < array.length do
-    if max[value] < array[count]
+    if highest_mumber < array[count]
       highest_number = array[count]
     end
   count += 1
   end
   highest_number
 end
-
-
-#pseudocode
-# find the lenght of the array to know how many times to loop through
-#set highest_number = 0
-#while the count < the length of the array
-# find out what the value of each index is and convert it to an integer
